01e8bbf05e77f17ff02fa65c53d1af0d9cb4a37d
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2025-01-24 14:03:41 +0800

    feat: uv

--- eval/eval_qa.py
+++ eval/eval_qa.py
@@ -12,14 +12,22 @@ import tiktoken
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--test_data_name", type=str, default=None, help="Test data name")
-    parser.add_argument("--test_data_split", type=str, default=None, help="Test data split")
-    parser.add_argument("--output_file_path", type=str, default=None, help="Output file")
+    parser.add_argument(
+        "--test_data_name", type=str, default=None, help="Test data name"
+    )
+    parser.add_argument(
+        "--test_data_split", type=str, default=None, help="Test data split"
+    )
+    parser.add_argument(
+        "--output_file_path", type=str, default=None, help="Output file"
+    )
     parser.add_argument("--reader_model", type=str, default=None, help="Reader model")
 
     args = parser.parse_args()
 
-    test_data = load_dataset("TIGER-Lab/LongRAG", args.test_data_name, split=args.test_data_split)
+    test_data = load_dataset(
+        "TIGER-Lab/LongRAG", args.test_data_name, split=args.test_data_split
+    )
     if args.reader_model == "GPT-4o":
         llm_inference = GPTInference()
     elif args.reader_model == "Gemini":
@@ -27,7 +35,7 @@ if __name__ == "__main__":
     elif args.reader_model == "Claude":
         llm_inference = ClaudeInference()
 
-    output_file = open(args.output_file_path, 'w')
+    output_file = open(args.output_file_path, "w")
     enc = tiktoken.get_encoding("cl100k_base")
     substring_match, exact_match, retrieval = 0, 0, 0
     tt = 0
@@ -41,9 +49,13 @@ if __name__ == "__main__":
         context_sizes.append(context_size)
         try:
             if args.test_data_name == "nq":
-                long_ans, short_ans = llm_inference.predict_nq(context, question, context_titles)
+                long_ans, short_ans = llm_inference.predict_nq(
+                    context, question, context_titles
+                )
             elif args.test_data_name == "hotpot_qa":
-                long_ans, short_ans = llm_inference.predict_hotpotqa(context, question, context_titles)
+                long_ans, short_ans = llm_inference.predict_hotpotqa(
+                    context, question, context_titles
+                )
         except:
             long_ans, short_ans = "", ""
         is_exact_match = single_ans_em(short_ans, answers)
@@ -52,7 +64,9 @@ if __name__ == "__main__":
         if args.test_data_name == "nq":
             is_retrieval = has_correct_answer(context, answers)
         else:
-            is_retrieval = (item["sp"][0] in context_titles) and (item["sp"][1] in context_titles)
+            is_retrieval = (item["sp"][0] in context_titles) and (
+                item["sp"][1] in context_titles
+            )
 
         output = {
             "query_id": item["query_id"],
